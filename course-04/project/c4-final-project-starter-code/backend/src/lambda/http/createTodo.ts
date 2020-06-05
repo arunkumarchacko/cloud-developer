5db38fa9da3aa6dc50d8a236226ba5c0f2776dba
Arunkumar Chacko <acchack@amazon.com>
2020-06-05 12:45:34 +0530

    Create resources

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -4,20 +4,63 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLENAME
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
   console.log(newTodo)
-  // TODO: Implement creating a new TODO item
-  // return undefined
+  console.log('Processing event: ', event)
+  
+  const parsedBody = JSON.parse(event.body)
 
-  return {
+  console.log('Parsed event: ', parsedBody)
+
+  // const authorization = event.headers.Authorization
+  // const split = authorization.split(' ')
+  // const jwtToken = split[1]
+
+  const jwtToken = "todo"
+  
+  const itemId = uuid.v4()
+  const timestamp = new Date().toISOString() 
+
+  const newItem = {
+    todoId: itemId,
+    userId: getUserId(jwtToken),
+    timestamp: timestamp,
+    ...parsedBody
+  }
+
+  console.log('Storing new item: ', newItem)
+
+  await docClient.put({
+    TableName: todoTable,
+    Item: newItem
+  }).promise()
+
+  console.log('Created item: ', newItem)
+  
+  const response = {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      newItem: newTodo
+      newItem
     })
   }
+
+  console.log('Response: ', response)
+
+  return response
+}
+
+function getUserId(jwtToken: string) {
+  return "TODO" + jwtToken
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -2,14 +2,28 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import * as AWS  from 'aws-sdk'
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const tableName = process.env.TODO_TABLENAME
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
-  console.log(event)
+  console.log("Getting todo items:" + event)
+  const result = await docClient.scan({
+    TableName: tableName
+  }).promise()
+
+  const items = result.Items
+
+  // TODO: Filter for a user
   return {
-    statusCode: 404,
+    statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: ''
+    body: JSON.stringify({
+      items
+    })
   }
 }
